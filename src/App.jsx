df7fd8abdf8fabe3386c0fafb92730aac126e568
Sweta Suman <swetaaaaa.suman12345@gmail.com>
2024-06-13 15:17:32 +0530

    todolistreact

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TodoWrapper from "./components/TodoWrapper";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoWrapper.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import TodoForm from "./TodoForm";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Todo from "./Todo";
+import EditTodoForm from "./EditTodoForm";
+
+const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+    console.log(todos); // This will log the previous state due to closure. If you want to see the updated state, use useEffect.
+  };
+
+  const toggleComplete = (id) => {
+    // Fix syntax issue with brackets
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <h1>Get Things Done 🙌 </h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            task={todo}
+            key={index}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+export default TodoWrapper;
